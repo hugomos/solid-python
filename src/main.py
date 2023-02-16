a1e21316e4d64135e46aaad5f1c4d1c150f6c523
Vitor <vitor_osantos@hotmail.com>
2023-02-16 09:45:12 -0300

    feat: adding runtime variables to main file. @hugomos

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from useCases.createUser.index import (create_user_use_case, create_user_controller)
 from entities.user import User
+from useCases.createUser.create_user_dto import ICreateUserDTO
 
-user = 
+user = ICreateUserDTO(username='vitor_osantos', email='vitor_osantos@hotmail.com')
+
+create_user_controller.handle(user)
